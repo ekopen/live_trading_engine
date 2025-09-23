a01ca48b55b17e1ce2e2612e9866d2f2b8e48850
Eric Kopen <33678196+ekopen@users.noreply.github.com>
2025-09-23 15:33:42 -0400

    cleaning up config

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 # config.py
 # variables that are used across the project
 
-import os
-
 KAFKA_BOOTSTRAP_SERVER="159.65.41.22:9092"
 
-CLICKHOUSE_DURATION = (60 * 60 * 23)  # how old data can be in Clickhouse before it will be moved to a parquet (seconds)
-ARCHIVE_FREQUENCY = (60 * 60 * 1)  # how often we check for old data to move to parquet and upload to cloud (seconds)
-HEARTBEAT_FREQUENCY = 30 # seconds per heartbeat, where we record diagnostics and monitoring data 
-
-EMPTY_LIMIT = 10 #number of consecutive empty diagnostics records before restarting the system
+CLICKHOUSE_DURATION = (60 * 60 * 24 * 7)  # how old data can be in Clickhouse before it will be moved to a parquet (seconds)
+ARCHIVE_FREQUENCY = (60 * 60 * 24)  # how often we check for old data to move to parquet and upload to cloud (seconds)
+HEARTBEAT_FREQUENCY = 60 # seconds per heartbeat, where we record diagnostics and monitoring data 
 
 WS_LAG_THRESHOLD = 1.5 # amount of websocket lag considered a spike
 PROC_LAG_THRESHOLD = 3 # amount of processing lag considered a spike
