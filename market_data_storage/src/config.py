62b85c74cf05aa05941352e82a21b9a480080612
Eric Kopen <erictkopen@gmail.com>
2025-10-18 04:03:28 +0000

    updating delete configuration

--- market_data_storage/src/config.py
+++ market_data_storage/src/config.py
@@ -3,7 +3,7 @@ import os
 
 KAFKA_BOOTSTRAP_SERVER="kafka:19092"
 
-CLICKHOUSE_DURATION = (60 * 60 * 24 * 7)  # how old data can be in Clickhouse before it will be moved to a parquet (seconds)
+CLICKHOUSE_DURATION = (60 * 60 * 24 * 30)  # how old data can be in Clickhouse before it will be moved to a parquet (seconds)
 ARCHIVE_FREQUENCY = (60 * 60 * 24)  # how often we check for old data to move to parquet and upload to cloud (seconds)
 HEARTBEAT_FREQUENCY = 60 # seconds per heartbeat, where we record diagnostics and monitoring data 
 
